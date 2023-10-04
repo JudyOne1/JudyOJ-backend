0b25501601b0f061da62441a3d893427a05b982c
Judy <mzyy867734122@163.com>
2023-10-04 22:26:12 +0800

    init by Judy

--- src/main/java/com/yupi/JudyOJ/service/PostFavourService.java
+++ src/main/java/com/yupi/JudyOJ/service/PostFavourService.java
@@ -11,8 +11,8 @@ import com.yupi.JudyOJ.model.entity.User;
 /**
  * 帖子收藏服务
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author Judy 
+ *  
  */
 public interface PostFavourService extends IService<PostFavour> {
 
--- src/main/java/com/yupi/JudyOJ/service/PostThumbService.java
+++ src/main/java/com/yupi/JudyOJ/service/PostThumbService.java
@@ -7,8 +7,8 @@ import com.yupi.JudyOJ.model.entity.User;
 /**
  * 帖子点赞服务
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author Judy 
+ *  
  */
 public interface PostThumbService extends IService<PostThumb> {
 
